fa26e3d5a89dbc6f95a4bcf9e3a105edbc16805b
John Swanson <john.d.swanson@gmail.com>
2016-10-12 16:20:16 -0700

    simplify view code

--- src/cljs/recipe/views.cljs
+++ src/cljs/recipe/views.cljs
@@ -5,7 +5,7 @@
             [markdown.core :refer [md->html]]))
 
 (defn input'
-  [& {:keys [save change stop value]}]
+  [{:keys [save change stop value]}]
   [:input {:value value
            :on-key-down #(condp = (.-which %)
                            13 (do (save) (stop))
@@ -17,8 +17,10 @@
 (def input (with-meta input' {:component-did-mount #(.focus (reagent/dom-node %))}))
 
 (defn textarea'
-  [& {:keys [save change stop value]}]
+  [{:keys [save change stop value]}]
   [:textarea {:value value
+              :style {:width "100%"
+                      :height "500px"}
               :on-key-down #(cond
                               (and (= (.-which %) 13)
                                    (.-shiftKey %)) (do (save) (stop))
@@ -42,100 +44,6 @@
    (for [id ids]
      ^{:key id} [:div id])])
 
-(defn title-input [& {:keys [value on-key-down on-change stop]}]
-  [:input {:value value
-           :on-key-down on-key-down
-           :on-change on-change
-           :on-blur stop}])
-
-(def title-edit (with-meta title-input {:component-did-mount #(.focus (reagent/dom-node %))}))
-
-(defn title-editor
-  [orig-title & {:keys [on-save]}]
-  (let [state (reagent/atom {:title orig-title
-                             :editing? false})
-        start #(swap! state assoc
-                      :editing? true
-                      :needs-focus? true)
-        stop #(swap! state assoc :editing? false)
-        temp-save #(swap! state assoc :title %)
-        save #(let [v (-> @state :title str clojure.string/trim)]
-                (when (and on-save (seq v)) (on-save v))
-                (stop))
-        on-change #(temp-save (.. % -target -value))
-        on-key-down #(case (.-which %)
-                       13 (save)
-                       27 (stop)
-                       nil)]
-    (fn [orig-title & {:keys [on-save on-stop]}]
-      (let [{:keys [title editing?]} @state]
-        [:div
-         (if editing?
-           [title-edit :value title
-            :on-key-down on-key-down
-            :on-change on-change
-            :stop stop]
-           [:span
-            {:on-click #(swap! state assoc :editing? true)}
-            (or title
-                [:i "title"])])
-         (if (and (not editing?)
-                  (not= title orig-title))
-           [:i {:on-click #(swap! state assoc :title orig-title)} "(pending, click to revert)"])]))))
-
-(defn description-input [& {:keys [value on-key-down on-change stop]}]
-  [:textarea {:value value
-           :on-key-down on-key-down
-           :on-change on-change
-           :on-blur stop}])
-
-(def description-edit (with-meta description-input {:component-did-mount #(.focus (reagent/dom-node %))}))
-
-(defn description-editor
-  [{:keys [value on-save]}]
-  (let [state (reagent/atom {:value value
-                             :editing? false})
-        start #(swap! state assoc :editing? true)
-        stop #(swap! state assoc :editing? false)
-        save #(let [v (-> @state :value str clojure.string/trim)]
-                (when (seq v) (on-save v))
-                (stop))
-        on-change #(swap! state assoc :value (.. % -target -value))
-        on-key-down #(cond
-                       (and (= (.-which %) 13)
-                            (.-shiftKey %)) (save)
-                       (= (.-which %) 27) (stop)
-
-                       :else nil)]
-    (fn [{orig-value :value}]
-      (let [{:keys [value editing?]} @state]
-        [:div
-         (if editing?
-           [description-edit
-            :value value
-            :on-key-down on-key-down
-            :on-change on-change
-            :stop stop]
-           [:div {:on-click #(swap! state assoc :editing? true)}
-            (or value [:i "description"])])
-         (if (and (not editing?)
-                  (not= value orig-value))
-           [:i {:on-click #(swap! state assoc :value orig-value)} "(pending, click to revert)"])]))))
-
-(defn recipe-editor []
-  (let [editing (subscribe [:app/editing-recipe])]
-    (fn []
-      (let [{:keys [recipe/title
-                    recipe/ingredients
-                    recipe/steps
-                    recipe/description
-                    recipe/owner
-                    recipe/id]} @editing]
-        [:div "Recipe Editor"
-         [:div
-          [title-editor title :on-save #(dispatch [:update-editing-recipe :recipe/title %])]
-          [description-editor {:value description :on-save #(dispatch [:update-editing-recipe :recipe/description %])}]]]))))
-
 (defn recipe-importer
   []
   (let [v (reagent/atom "")]
@@ -149,26 +57,6 @@
 (defn save-button [url] [:button {:on-click #(dispatch [:save-import url])} "Save"])
 (defn cancel-button [url] [:button {:on-click #(dispatch [:cancel-import url])} "Cancel"])
 
-(defn title-view [url title]
-  (let [state (reagent/atom {:value title})
-        save #(dispatch [:import/update url :recipe/title (:value @state)])
-        stop #(swap! state assoc :editing? false)
-        save-local #(swap! state assoc :value (.. % -target -value))]
-    (fn [url title]
-      (let [{:keys [editing? value]} @state]
-        (if-not editing?
-          [:div
-           {:on-click #(swap! state assoc :editing? true)}
-           [:h2 "Title"]
-           title]
-          [:div
-           [:h2 "Title"]
-           [input
-            :value value
-            :save save
-            :stop stop
-            :change save-local]])))))
-
 (defn image-selector [url possible-images]
   [:div [:h3 "Images"] ;; image-selector
    (for [img possible-images]
@@ -177,24 +65,45 @@
                         :on-click #(dispatch [:import/select-image url img])
                         :src img}])])
 
-(defn note-editor
-  [url notes]
-  (let [state (reagent/atom {:value notes :editing? false})
-        save #(dispatch [:import/update url :recipe/notes (:value @state)])
+(defn markdown [value]
+  [:div {:dangerouslySetInnerHTML
+         {:__html (md->html value)}}])
+
+(defn editor
+  [{:keys [url value editor title markdown?]}]
+  (console.log url title editor)
+  (let [state (reagent/atom {:value value :editing? false})
+        save #(dispatch [:import/update url key (:value @state)])
         stop #(swap! state assoc :editing? false)
         save-local #(swap! state assoc :value (.. % -target -value))]
-    (fn [url notes]
+    (fn [{:keys [url value editor title markdown?]}]
+      (console.log url title editor)
       (let [{:keys [editing? value]} @state]
-        (if editing?
-          [:div
-           [:h3 "Notes"]
-           [textarea
-            :value value
-            :change save-local
-            :stop stop
-            :save save]]
-          [:div {:on-click #(swap! state assoc :editing? true)}
-           [:h3 "Notes"] notes])))))
+        [:div
+         [:h3 title]
+         (if editing?
+           [editor {:value value
+                    :change save-local
+                    :stop stop
+                    :save save}]
+           [:div {:on-click #(swap! state assoc :editing? true)}
+            (if markdown?
+              [markdown value]
+              value)])]))))
+
+(defn title-view
+  [url title]
+  [editor {:url url
+           :value title
+           :editor input
+           :title "Title"}])
+
+(defn note-editor
+  [url notes]
+  [editor {:url url
+           :value notes
+           :editor textarea
+           :title "Notes"}])
 
 (defn ingredient-editor
   [url ingredients]
@@ -203,10 +112,12 @@
           ^{:key i} [:li i])]])
 
 (defn procedure-editor
- [url procedure]
- [:div [:h3 "Procedure"]
- [:div {:dangerouslySetInnerHTML
-        {:__html (md->html procedure)}}]])
+  [url procedure]
+  [editor {:url url
+           :value procedure
+           :editor textarea
+           :title "Procedure"
+           :markdown? true}])
 
 (defn import-editor [{:keys [import/recipe
                              import/url
